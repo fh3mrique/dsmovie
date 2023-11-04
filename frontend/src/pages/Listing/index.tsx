bf4c26791c89e0508ebaf17b804bb61eeb1424d4
fh3mrique <97576330+fh3mrique@users.noreply.github.com>
2023-11-04 17:31:48 -0300

    UseState e UseEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -2,6 +2,8 @@ import Pagination from "../../components/Pagination";
 import MovieCard from "../../components/MovieCard";
 import axios from "axios";
 import { BASE_URL } from "../../utils/requests";
+import { useEffect, useState } from "react";
+import { MoviePage } from "../../types/movie";
 
 function Listing() {
 
@@ -11,16 +13,32 @@ function Listing() {
         console.log(response.data);       
   }); */
 
-  axios.get(`${BASE_URL}/movies?size=12&page=0`)
+  /* Define um estado 'pageNumber' e uma função 'setPageNumber' para 
+  manipulá-lo. Inicializa 'pageNumber' com o valor 0.*/
+  const [pageNumber, setPageNumber] = useState(0);
+
+  /*O hook useEffect é usado para executar um efeito colateral após a renderização do componente. 
+  O efeito colateral é uma chamada de API usando Axios para obter dados de filmes */
+  useEffect (() => {
+    axios.get(`${BASE_URL}/movies?size=12&page=1`)
+    // Quando a resposta da requisição estiver disponível, esta função de retorno será executada.
       .then(response => {
-        console.log(response.data);       
+
+        // Extrai os dados da resposta da requisição e os converte para o tipo 'MoviePage'.
+        const data = response.data as MoviePage;
+
+        console.log(data )
+        // Define o estado 'pageNumber' com o valor obtido da resposta (neste caso, o valor de 'data.number').
+        setPageNumber(data.number);
   });
+  }, [])
 
 
   return (
     <>
       <Pagination />
 
+      <p>{pageNumber}</p>
       <div className="container">
         <div className="row">
           <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
