18e0c96284a70a724e899185a9a0a2f2a74f9260
fh3mrique <97576330+fh3mrique@users.noreply.github.com>
2023-11-04 11:13:25 -0300

    Domain model, database seed

---
+++ backend/src/main/java/com/pessoal/dsmovie/entities/Score.java
@@ -0,0 +1,44 @@
+package com.pessoal.dsmovie.entities;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_score")
+public class Score {
+
+	@EmbeddedId
+	private ScorePK id = new ScorePK();
+
+	private Double value;
+
+	public Score() {
+	}
+
+	//set especial para associar um filme ao score
+	public void setMovie(Movie movie) {
+		id.setMovie(movie);
+	}
+
+	//set especial para associar um usuario ao score
+	public void setUser(User user) {
+		id.setUser(user);
+	}
+
+	public ScorePK getId() {
+		return id;
+	}
+
+	public void setId(ScorePK id) {
+		this.id = id;
+	}
+
+	public Double getValue() {
+		return value;
+	}
+
+	public void setValue(Double value) {
+		this.value = value;
+	}
+}
