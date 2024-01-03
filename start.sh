a76d17f9a29c300ca980ed51b77cacb3a2c46bd7
fh3mrique <97576330+fh3mrique@users.noreply.github.com>
2024-01-03 03:46:31 -0300

    Automatização de build com shellscript 2

--- start.sh
+++ start.sh
@@ -1,16 +1,30 @@
 #!/bin/bash
 
-echo 'Iniciando frontend....'
-cd frontend
+echo "Qual stack você quer rodar?"
+echo "1 - Stack Japão: React no front com Java Spring no back."
+echo "2 - Stack Mobile: Flutter e node js"
+read -p "Digite o número correspondente à sua escolha: " opcao
 
-npm install 
-npm run dev &
-
-sleep 10
-
-echo 'Levando API....'
-cd ../backend
-
-./mvnw spring-boot:run &
-
-echo 'Processo finalizado'
+if [ "$opcao" == "1" ]; then
+    # Stack Japão
+    echo "Iniciando a Stack Japão..."
+    
+    echo "Passo 1: Iniciando o frontend..."
+    cd frontend
+    npm install
+    npm run dev &
+    sleep 10
+    
+    echo "Passo 2: Levando a API..."
+    cd ../backend
+    ./mvnw spring-boot:run &
+    
+    echo "Processo finalizado para a Stack Japão."
+elif [ "$opcao" == "2" ]; then
+    # Stack Mobile
+    echo "Iniciando a Stack Mobile..."
+    echo "Uma aplicação Flutter e mobile deverá ser iniciada."
+    # Incluir aqui comandos ou mensagens relacionados ao Flutter e Node.js
+else
+    echo "Opção inválida. Por favor, escolha 1 ou 2."
+fi
